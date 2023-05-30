faf541851e7e49b00c4a0db3fb942d9e59db8bcd
1946735 <1946735@naver.com>
2023-05-31 08:53:38 +0900

    Initial Fire Commit

---
+++ vtacctmaint/src/main/java/com/example/demo/VtacctmaintApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtacctmaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtacctmaintApplication.class, args);
+	}
+
+}
